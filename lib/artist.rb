94e11e2638864897d971d86207467dc4bc056a98
Wasif Khan <waasif.khaan@gmail.com>
2018-05-17 04:00:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :songs
 
@@ -18,9 +19,14 @@ class Artist
   def self.all
     @@artists
   end
+    # def self.reset_all
+  #   self.all.clear
+  # end
 
+  # def self.count
+  #   @@artists.count
+  # end
   
-
   def add_song(song)
     @songs << song
     song.artist = self
--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,12 @@
 module Memorable 
+  module ClassMethods
   
-  def self.reset_all
+  def reset_all
     self.all.clear
   end
 
-  def self.count
-    @@artists.count
+  def count
+    self.all.count
   end
+end 
 end 
